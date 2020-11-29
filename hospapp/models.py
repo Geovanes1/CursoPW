cba930d9345f0f00dbb3d66d2bc9ef109b545d1f
Geovane <gbstech90@gmail.com>
2020-11-29 20:29:04 -0300

    Definindo Modelos

--- hospapp/models.py
+++ hospapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
+class Hospital (models.Model):
+
+	nome_hospital = models.CharField(max_length=200)
+	foto = models.ImageField(null=True, blank=200)
+	desc_hospital = models.CharField(max_length=200)
+	tipo_hospital = models.CharField(max_length=200)
+	conceito_hospital = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.nome_hospital
