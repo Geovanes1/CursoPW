5e33f7ef7d2d143ac94eebeb5ca3d92e004e6f58
Geovane <gbstech90@gmail.com>
2020-11-30 03:49:26 -0300

    Ajustes de espaco

--- hospitais/urls.py
+++ hospitais/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hospapp.views import index, hospital
+from hospapp.views import index, hospital 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
