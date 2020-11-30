ad1c79f88e6934883fcd86538ee32a4b350e6c32
Geovane <gbstech90@gmail.com>
2020-11-29 21:10:00 -0300

    Iniciando templetes 01

--- hospapp/views.py
+++ hospapp/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Aqui é index<h1>")
+    #return HttpResponse("<h1>Aqui é index<h1>")
+    return render(request, 'hospitais/index.html')
 
 def hospitais(request):
     return HttpResponse("<h1>Aqui é area Hospital<h1>")
