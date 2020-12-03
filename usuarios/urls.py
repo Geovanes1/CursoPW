5fd0db5d64244b6fcc17f198984c56540253a049
Geovane <gbstech90@gmail.com>
2020-12-03 01:56:27 -0300

    Cadastro de Usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_view
-
+from .views import criar_usuario
 
 
 urlpatterns=[
@@ -10,5 +10,8 @@ path('login/', auth_view.LoginView.as_view(
 	name='login'),
 
 path('logout/', auth_view.LogoutView.as_view(),
-	name='logout')
+	name='logout'),
+path('criar_usuario/', criar_usuario, name='criar_usuario'),
+
+
 ]
